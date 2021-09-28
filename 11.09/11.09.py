926e91ee43bb39f6d4604d8a76ba0e283323c04f
emkramer <89976171+emkramer@users.noreply.github.com>
2021-09-28 15:14:50 +0300

    Декоратор 10.09

--- 11.09/11.09.py
+++ 11.09/11.09.py
@@ -1,44 +1,25 @@
-import random
 import string
-s = string.ascii_letters
-n = ''
-l = [c for c in s]
-def cr_str(width):
-    l = [random.choice(string.ascii_letters) for i in range(width)]
-    s = ''
-    for el in l:
-        s += el
-    return s
-def cr_list_str(width, num):
-    l = [cr_str(width) for i in range(num) ]
-    s = ''
-    big = 0
-    small = 0
-    statb = 0
-    stats = 0
-    statr = 0
-    for el in l:
-        s += el
-        s += ' '
-        for i in el:
-            if i.isupper():
-                big += 1
-            else:
-                small += 1
-        if big > small:
-            statb += 1
-            s += '1'
-        elif small > big:
-            stats += 1
-            s += '0'
-        else:
-            statr += 1
-            s += '-1'
-        
-        s += ' '
-    s += '> A ' + str(statb/(statb+stats+statr)*100) + '% '
-    s += '> a ' + str(stats/(statb+stats+statr)*100) + '% '
-    s += 'a == A ' + str(statr/(statb+stats+statr)*100) + '%'
-    return s
+s = string.ascii_lowercase
 
-print(cr_list_str(8, 8),)
+l = [c for c in s]
+
+def f2(l):
+    i=0
+    while i < len(l):
+        print(l[i])
+        i+=1
+
+#f2(l)
+def dec(leng):
+    def new_leng(*args,**kwargs):
+        print('Running function: ', leng.__name__)
+        print('Positional arguments are: ', args)
+        print('Keyword arguments are: ', kwargs)
+        result = leng(*args, **kwargs)
+        print('Result: ', result)
+        return result
+    return new_leng
+
+f2_dec = dec(f2)
+
+f2_dec(l)
